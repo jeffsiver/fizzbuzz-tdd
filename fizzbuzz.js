a856a6f7cbd1093f98c59482dd034e85a5bee1e0
Jeffrey Siver <jeff@siverfamily.com>
2022-03-29 22:38:34 -0500

    starting point

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,33 +2,3 @@
 // When a number is divisible by 3, replace the number with fizz
 // When a number is divisible by 5, replace the number with buzz
 // When a number is divisible by both, replace the number with fizzbuzz
-
-
-class Fizzbuzz {
-    _fizzbuzzConverter;
-    constructor(fizzbuzzConverter = new FizzbuzzConverter()) {
-        this._fizzbuzzConverter = fizzbuzzConverter;
-    }
-
-    generate(numberOfEntries) {
-        const result = [];
-        for(let number = 1; number <= numberOfEntries; number++) {
-            result.push(this._fizzbuzzConverter.convertNumberToString(number));
-        }
-        return result;
-    }
-}
-
-class FizzbuzzConverter {
-    convertNumberToString(number) {
-        if (number % 3 === 0 && number % 5 === 0)
-            return 'fizzbuzz';
-        if (number % 3 === 0)
-            return 'fizz';
-        if (number % 5 === 0)
-            return 'buzz'
-        return number.toString();
-    }
-}
-
-module.exports = { Fizzbuzz, FizzbuzzConverter }
